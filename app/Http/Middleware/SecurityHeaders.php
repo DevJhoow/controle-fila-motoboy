c38c36acb18bd997cb1161a41d04d5d78c5d3d39
DevJhoow <jdk99.rodrigues@gmail.com>
2026-02-15 12:57:31 -0300

    ajuste SecurityHeaders

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -9,35 +9,27 @@ use Symfony\Component\HttpFoundation\Response;
 class SecurityHeaders
 {
         public function handle(Request $request, Closure $next): Response
-        {
-            $response = $next($request);
+       {
+        $response = $next($request);
 
-            // COOP
-            $response->headers->set(
-                'Cross-Origin-Opener-Policy',
-                'same-origin'
-            );
+        // COOP
+        $response->headers->set(
+            'Cross-Origin-Opener-Policy',
+            'same-origin'
+        );
 
-            // CSP + Trusted Types (resolve XSS DOM)
-            $response->headers->set(
-                'Content-Security-Policy',
-                "default-src 'self';
-                script-src 'self' https://cdn.jsdelivr.net;
-                style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;
-                img-src 'self' data:;
-                font-src 'self' https://cdn.jsdelivr.net;
-                connect-src 'self';
-                frame-ancestors 'none';
-                base-uri 'self';
-                form-action 'self';
-                require-trusted-types-for 'script';"
-            );
+        // Clickjacking
+        $response->headers->set(
+            'X-Frame-Options',
+            'DENY'
+        );
 
-            //  Proteções extras (boa prática)
-            $response->headers->set('X-Frame-Options', 'DENY');
-            $response->headers->set('X-Content-Type-Options', 'nosniff');
-            $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
+        // CSP (UMA LINHA SÓ)
+        $response->headers->set(
+            'Content-Security-Policy',
+            "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; script-src 'self' https://cdn.jsdelivr.net; frame-ancestors 'none';"
+        );
 
-            return $response;
-        }
+        return $response;
+    }
 }
